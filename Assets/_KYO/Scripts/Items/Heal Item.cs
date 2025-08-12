3bb0aa953d8968147356d7a3a79c76f378422635
yybbuu1 <ybu1357@gmail.com>
2025-08-12 09:54:56 +0900

    8/12
    플레이어 기능/ 아이템 획득(후레쉬, 회복물약)/UI 프리팹/아이템 프리팹/

---
+++ Assets/_KYO/Scripts/Items/Heal Item.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "HealItem", menuName = "Scriptable Object/Items/HealItem")]
+public class HealItem : ItemDatas
+{ // 스크립터블. item data 형식에 참조되어있음
+    public int HealMentalHP = 50;
+
+    public void Use(PlayerStatus playerstatus)
+    {
+        if (playerstatus != null)
+        {
+            playerstatus.HealMentalHP(HealMentalHP);
+            Debug.Log("냠냠 회복중");
+        }
+    }
+
+    protected override void Reset()
+    {
+        
+        type = ItemType.Consumable;
+        itemName = "물병";
+        description = "정신력을 회복시켜주는 시원한 물병";
+        HealMentalHP = 50;
+
+    }
+}
---
+++ Assets/_KYO/Scripts/Player/ItemPickUp.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemPickUp : MonoBehaviour
+{ //플레이어에게 붙임
+    public Camera cam;
+    public float pickUpRange = 3f;
+    public PlayerInventory inventory;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Ray ray = new Ray(cam.transform.position, cam.transform.forward); //ray를 앞으로 쏨
+            if (Physics.Raycast(ray, out RaycastHit hit, pickUpRange))
+            {//ray를 쏴서 거리내에 뭔가 닿았다면 hit에 값을 저장
+                ItemObject item = hit.collider.GetComponent<ItemObject>(); //뭔가 닿은게 itemobject잇는지 검사
+                if (item != null)
+                {
+                    inventory.AddItem(item.data); //인벤토리에 아이템 추가
+
+                    if (item.data.type == ItemType.Flash) //손전등일경우
+                    {
+                        FlashLight flashLight = this.GetComponent<FlashLight>(); //flash 컴포넌트 가져옴
+                        if (flashLight != null)
+                        {
+                            flashLight.EquipFlashLight(item.data.modelPrefab); //손전등 컴포넌트가 있으면 장착
+                        }
+                    }
+                    Destroy(item.gameObject); //주우면 사라짐
+                    Debug.Log($"{item.data.itemName} 획득");
+                    
+                }
+            }
+        }
+
+      
+    }
+
+}
