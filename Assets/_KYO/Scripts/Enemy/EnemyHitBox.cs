f0d555ce7bda6042eb96d0aa3f33ab177a0d645d
yybbuu1 <shot135@naver.com>
2025-08-21 13:42:16 +0900

    Ybu (#38)
    * 8/21 병합
    
    몬스터 공격데미지 구현/게임매니저 아이템저장 및 인벤토리 아이템이동 삭제/
    
    * 머지
    
    ---------
    
    Co-authored-by: yybbuu1 <ybu1357@gmail.com>

---
+++ Assets/_KYO/Scripts/Enemy/EnemyHitBox.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class EnemyHitBox : MonoBehaviour
+{
+    [SerializeField] private int mentalDamage = 50;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            PlayerStatus status = other.GetComponent<PlayerStatus>();
+            if (status != null)
+            {
+                status.HealMentalHP(-mentalDamage);
+                Debug.Log($"정신력 {mentalDamage} 감소!");
+            }
+        }
+    }
+}
+
