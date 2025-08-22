56ab16c997d50efe69619d9703c0f7a4967aa6a7
Huroad <cabin9999@naver.com>
2025-08-22 14:27:36 +0900

    모니터사운드, 마우스감도 적용

--- Assets/_KYO/Scripts/Player/PlayerCamera.cs
+++ Assets/_KYO/Scripts/Player/PlayerCamera.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Google.Protobuf;
 using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
@@ -9,16 +10,19 @@ public class NewBehaviourScript : MonoBehaviour
     float xRotation = 0f;
     public Transform flashlight; // 손전등 Transform 추가
 
+    private GameSetting settings;
 
     void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
+        settings = UIManager.Instance.Settings;
     }
 
     void Update()
     {
-        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
-        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+        float sens = settings.mouseSensitivity;
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime * sens;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime * sens;
 
         xRotation -= mouseY;
         xRotation = Mathf.Clamp(xRotation, -90f, 90f);
